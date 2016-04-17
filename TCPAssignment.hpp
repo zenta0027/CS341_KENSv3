857645fea04899c54a0d8c31b213a9b3b5eab87a
Zentroll <zenta0027@kaist.ac.kr>
2016-04-18 04:52:53 +0900

    Adding TCP-header struct

--- TCPAssignment.hpp
+++ TCPAssignment.hpp
@@ -22,12 +22,40 @@
 namespace E
 {
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////                                    TCP Header                               ////////////////////////////////
+////             Source Port(16bits)                       /////////////////////             Destination Port(16bits)           /////////
+////                                                        Sequence Number(32bits)                                             /////////
+/// Data Offset(4bits) // Reserved(6bits, 000000) // Control Bits(6bits) ///                 Windows(16bits)                    /////////
+////              Checksum(16bits)                          ////////////////////               Urgent Pointer(16bits)           /////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct TCPHeader {
+	uint16_t src_port; //source port
+	uint16_t dst_port; //destination port
+	uint32_t sequence_num; //sequence number
+	uint32_t ack_num; //acknowledgement number
+
+	uint8_t offset; //offset
+
+	//control bits;
+	uint8_t urg; // & 0x20
+	uint8_t ack; // & 0x10
+	uint8_t psh; // & 0x08
+	uint8_t rst; // & 0x04
+	uint8_t syn; // & 0x02
+	uint8_t fin; // & 0x01
+	 
+	uint16_t window; //window
+	uint16_t checksum; //checksum
+	uint16_t urg_ptr; //urgent pointer
+};
+
 struct SocketData
 {
 	UUID socketUUID;
 	int fd;
 	int pid;
-	uint8_t sa_family;
 	uint8_t sin_family;
 	uint16_t sin_port;
 	struct in_addr sin_addr;
